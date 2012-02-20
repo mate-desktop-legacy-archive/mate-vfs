1c18409e1a2a25707480564cd4ee5ea100eb8cf4
Stefano Karapetsas <stefano@karapetsas.com>
2012-02-20 13:04:53 +0100

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -8,14 +8,13 @@ PKG_NAME="mate-vfs"
 REQUIRED_AUTOMAKE_VERSION=1.6
 
 (test -f $srcdir/configure.in \
-  && test -f $srcdir/HACKING \
   && test -d $srcdir/libmatevfs) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-which mate-autogen.sh || {
+which mate-autogen || {
     echo "You need to install mate-common from the MATE CVS"
     exit 1
 }
@@ -23,4 +22,4 @@ which mate-autogen.sh || {
 REQUIRED_AUTOMAKE_VERSION=1.8
 USE_MATE2_MACROS=1
 
-. mate-autogen.sh
+. mate-autogen
