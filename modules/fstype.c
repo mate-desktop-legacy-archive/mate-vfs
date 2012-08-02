9ff65ecfbb36675c1efb430fa3bace03c79f964d
Steve Zesch <stevezesch2@gmail.com>
2012-08-01 22:19:13 -0400

    Fixed incorrect fsf address. Closes issue #3

--- modules/fstype.c
+++ modules/fstype.c
@@ -1,21 +1,5 @@
 /* fstype.c -- determine type of filesystems that files are on
-   Copyright (C) 1990, 91, 92, 93, 94 Free Software Foundation, Inc.
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
-
-/* This code was relicensed by the FSF on May 1 2002
+   Copyright (C) 2002 Free Software Foundation, Inc.
  
    This file is free software; you can redistribute it and/or
    modify it under the terms of the GNU Library General Public License as
@@ -29,10 +13,10 @@
 
    You should have received a copy of the GNU Library General Public
    License along with the Mate Library; see the file COPYING.LIB.  If not,
-   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-   Boston, MA 02111-1307, USA.
- */
-/* Written by David MacKenzie <djm@gnu.ai.mit.edu>. */
+   write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+
+   Written by David MacKenzie <djm@gnu.ai.mit.edu>. */
 
 #include <config.h>
 #include <stdio.h>
